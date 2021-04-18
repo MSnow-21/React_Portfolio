2d4e8ae1511b7dbd887605b71838795c040c9578
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-18 11:15:57 -0400

    Import images to Applications.js. Created img tags with src definitions for images. Added additional jsx html

--- src/components/Applications.js
+++ src/components/Applications.js
@@ -1,4 +1,10 @@
 import React from "react";
+import BitsNBytes from "../assets/imgs/BitsnBytes.PNG";
+import TheWeatherDashboard from "../assets/imgs/WeatherDashboardMain.PNG";
+import WorkDayPlanner from "../assets/imgs/workdayplanner.PNG";
+import NoteTaker from "../assets/imgs/notetaker.PNG";
+import CodeQuiz from "../assets/imgs/codequiz.PNG";
+import TheWolf from "../assets/imgs/Wolf.PNG";
 
 function Applications() {
     return (
@@ -9,18 +15,26 @@ function Applications() {
             <div className="row">
                 <div className="col-sm-4">
                     <p><strong>Bits N Bytes</strong></p>
-                    <p>Coverts US Dollars to other currencies</p>
-                    <p>Allows user to pick items from menu</p>
-                    <p>Features a joke API</p>
+                        <img src={BitsNBytes} className="img-circle person" alt="BitsNBytes" width="250" height="250"></img>
+                        <br></br>
+                        <p><strong>Details</strong></p>
+                        <p>Coverts US Dollars to other currencies</p>
+                        <p>Allows user to pick items from menu</p>
+                        <p>Features a joke API</p>
+                        <p><a href="https://ambrosiomaxwell.github.io/Project1/">BitsnBytes</a></p>
                 </div>
                 <div className="col-sm-4">
                     <p><strong>The Weather Dashboard</strong></p>
+                    <img src={TheWeatherDashboard} className="img-circle person" alt="TheWeatherDashboard" width="250" height="250"></img>
+                    <p><strong>Details</strong></p>
                     <p>Uses a Weather API</p>
                     <p>Javascript and AJAX</p>
                     <p>Current Day and Five Day Forecast</p>
                 </div>
                 <div className="col-sm-4">
                     <p><strong>Work Day Scheduler</strong></p>
+                    <img src={WorkDayPlanner} className="img-circle person" alt="WorkDayPlanner" width="250" height="250"></img>
+                    <p><strong>Details</strong></p>
                     <p>Plan your day</p>
                     <p>Save notes with local storage</p>
                     <p>Refreshes each day</p>
@@ -29,18 +43,24 @@ function Applications() {
             <div className="row">
             <div className="col-sm-4">
                     <p><strong>Note Taker</strong></p>
+                    <img src={NoteTaker} className="img-circle person" alt="NoteTaker" width="250" height="250"></img>
+                    <p><strong>Details</strong></p>
                     <p>Create a Note and a Task</p>
                     <p>Stores Notes</p>
                     <p>Deletes Notes</p>
                 </div>
                 <div className="col-sm-4">
                     <p><strong>Code Quiz</strong></p>
+                    <img src={CodeQuiz} className="img-circle person" alt="CodeQuiz" width="250" height="250"></img>
+                    <p><strong>Details</strong></p>
                     <p>Test your Javascript knowledge</p>
                     <p>Use Javascript to create HTML elements</p>
                     <p>A timed Event</p>
                 </div>
                 <div className="col-sm-4">
                     <p><strong>The Wolf</strong></p>
+                    <img src={TheWolf} className="img-circle person" alt="TheWolf" width="250" height="250"></img>
+                    <p><strong>Details</strong></p>
                     <p>A website about my favorite animal</p>
                     <p>Allows the user to learn about wolves</p>
                     <p>Click to learn more</p>
