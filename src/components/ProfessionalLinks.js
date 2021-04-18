b05a7592dc44fe7161ff77e0920b72a3ca0ef7a7
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-04-18 10:08:14 -0400

    Moved images into src folder. Added imports to ProfessionalLinks.js component. Created tags with src definitions in Professional Links function.

--- src/components/ProfessionalLinks.js
+++ src/components/ProfessionalLinks.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Resume from "../assets/imgs/linkedin.png"
+import GithubImage from "../assets/imgs/github.png"
+
 
 function ProfessionalLinks() {
     return(
@@ -11,18 +14,21 @@ function ProfessionalLinks() {
             <div className="row text-center">
                 <div className="col-sm-4">
                     <div className="thumbnail">
+                        <img src={GithubImage} alt="Github"></img>
                         <p><strong>Github</strong></p>
                         <p>Projects and Activities</p>
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="thumbnail">
+                        <img src={Resume} alt="Resume"></img>
                         <p><strong>LinkedIn</strong></p>
                         <p>Professional Link</p>
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="thumbnail">
+                        <img src={Resume} alt="Resume" ></img>
                         <p><strong>Resume</strong></p>
                         <p>Professional Details</p>
                     </div>
